2ed320bfb6c560c08f46e23ed3008e74697fffa9
Yash Patel <yash.patel0419@gmail.com>
2022-07-28 19:51:42 -0400

    fixed require typo

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const path = requre('path');
+const path = require('path');
 const port = 3000;
 
 app.use(express.static('../templates'));
