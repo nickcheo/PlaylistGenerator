a1ba7d604c14cf7289324ae44f381b55b4ccd81d
Adam Kelch <askelch@gmail.com>
2022-07-28 20:37:07 -0400

    add server basics

--- myapp/app.js
+++ myapp/app.js
@@ -3,11 +3,19 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-app.use(express.static('../templates'));
+
+app.use('/images', express.static(path.join(__dirname, "../images")))
+
 
 app.get('/', (req, res) => {
 	// res.send('Hello World!')
 
+	res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+app.get('/testroute', (req, res) =>
+{
+	
 	res.sendFile(path.join(__dirname, '../public', 'genre.html'));
 });
 
