6637b319bef5a93f6014916999a8d6073b8956ed
Adam Kelch <askelch@gmail.com>
2022-08-06 20:01:11 -0400

    restore

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// rsetting back to when page routing and any tokens were working
 import Vue from 'vue'
 import App from './App'
 import router from './router'
