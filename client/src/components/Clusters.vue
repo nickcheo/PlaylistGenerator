97c4434ba5551d3e57907bec3063ffb101d7cf85
Adam Kelch <askelch@gmail.com>
2022-08-18 01:43:42 -0400

    new changes to heroki??

--- client/src/components/Clusters.vue
+++ client/src/components/Clusters.vue
@@ -260,8 +260,8 @@ import Api from '../services/Api';
           const state = params.get('state');
           const querystring = require('querystring')
 
-          const client_id= process.env.VUE_APP_CLIENT_ID
-            const client_secret=process.env.VUE_APP_CLIENT_SECRET
+          const client_id="a1c0d6debc2c49038fb8a43eb5df637a"
+          const client_secret="76669d3b28f94e8da7662d91cc39cc94"
 
           if(state == null)
             return null;
@@ -277,7 +277,7 @@ import Api from '../services/Api';
           body: querystring.stringify({
             grant_type: "authorization_code",
             code: authCode,
-            redirect_uri: process.env.VUE_APP_REDIRECT_URI,
+            redirect_uri: process.env.NODE_ENV != 'development' ? 'https://variefy.herokuapp.com' : 'http://localhost:8080',
           })
         });
 
@@ -515,8 +515,8 @@ import Api from '../services/Api';
     console.log("attempting refresh")
         if(getCookie("access_token") === "" )
         {
-              const client_id=process.env.VUE_APP_CLIENT_ID
-              const client_secret=process.env.VUE_APP_CLIENT_SECRET
+              const client_id="a1c0d6debc2c49038fb8a43eb5df637a"
+              const client_secret="76669d3b28f94e8da7662d91cc39cc94"
               const querystring = require('querystring')
           
           
