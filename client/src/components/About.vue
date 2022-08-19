e9298b2ecab28706ded48fd47790cb873d8e9169
Adam Kelch <askelch@gmail.com>
2022-08-19 11:32:52 -0400

    update pics

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -75,37 +75,53 @@
                         </div>
                      </div>
                 </div>
+        
                 <div class="container-fluid" id="developers">
-		<h2>Our Team </h2>
-		<div class="row">
-			<div class = "col-sm-3 text-center align-middle">
-				<img src = "../assets/yash.png"  class="rounded-circle about_img">
-				<p>Yash Patel<br>
-                <a href = 'https://linkedin.com/in/yash-patel-0389921a2'><i class="bi bi-linkedin"></i></a>
-                <a href = 'https://github.com/yashpatel21'><i class="bi bi-github"></i></a>
-                </p>				
-			</div>
-			<div class = "col-sm-3 text-center align-middle">
-				<img src = "../assets/adam.png"   class="rounded-circle about_img">
-				<p>Adam Kelch<br>
-                <a href = 'https://linkedin.com/in/adam-kelch'><i class="bi bi-linkedin"></i></a>
-                <a href = 'https://github.com/akelch11'><i class="bi bi-github"></i></a>
-                </p>
-			</div>
-			<div class = "col-sm-3 text-center align-middle">
-				<img src = "../assets/nick.png"  class="rounded-circle about_img">
-				<p>Nicholas Cheong<br>
-                <a href = 'https://linkedin.com/in/nickcheo'><i class="bi bi-linkedin"></i></a>
-                <a href = 'https://github.com/nickcheo'><i class="bi bi-github"></i></a></p>
-			</div>
-			<div class = "col-sm-3 text-center align-middle">
-				<img src = "../assets/rahul.png" class="rounded-circle about_img">
-				<p>Rahul Nalam<br><a href = 'https://linkedin.com/in/rahulnalam'><i class="bi bi-linkedin"></i></a>
-                <a href = 'https://github.com/rahulnalam'><i class="bi bi-github"></i></a>
-                </p>
-			</div>
-		</div>
-	</div>
+                    <h2>Our Team </h2>
+                        <div class="row">
+                            <div class = "col-sm-3 text-center align-middle">
+                                <img src = "../assets/yash.png"  class="rounded-circle about_img">
+                                <p>Yash Patel<br>
+                                <a href = 'https://linkedin.com/in/yash-patel-0389921a2'><i class="bi bi-linkedin"></i></a>
+                                <a href = 'https://github.com/yashpatel21'><i class="bi bi-github"></i></a>
+                                </p>				
+                            </div>
+                            <div class = "col-sm-3 text-center align-middle">
+                                <img src = "../assets/adam.png"   class="rounded-circle about_img">
+                                <p>Adam Kelch<br>
+                                <a href = 'https://linkedin.com/in/adam-kelch'><i class="bi bi-linkedin"></i></a>
+                                <a href = 'https://github.com/akelch11'><i class="bi bi-github"></i></a>
+                                </p>
+                            </div>
+                            <div class = "col-sm-3 text-center align-middle">
+                                <img src = "../assets/nick.png"  class="rounded-circle about_img">
+                                <p>Nicholas Cheong<br>
+                                <a href = 'https://linkedin.com/in/nickcheo'><i class="bi bi-linkedin"></i></a>
+                                <a href = 'https://github.com/nickcheo'><i class="bi bi-github"></i></a></p>
+                            </div>
+                            <div class = "col-sm-3 text-center align-middle">
+                                <img src = "../assets/rahul.png" class="rounded-circle about_img">
+                                <p>Rahul Nalam<br><a href = 'https://linkedin.com/in/rahulnalam'><i class="bi bi-linkedin"></i></a>
+                                <a href = 'https://github.com/rahulnalam'><i class="bi bi-github"></i></a>
+                                </p>
+                            </div>
+                        </div>
+	                </div>
+
+
+                
+                <div class="container-fluid" id="developers">
+                    <hr>
+                    <h2>Github Repo</h2>
+                    <p class="lead">Want to see how we built Variefy? Check it out here!</p>
+                    <br/>
+                    <a href = "https://github.com/nickcheo/PlaylistGenerator" target = "_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16" style= "color: white;">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                        </svg>
+                    </a>
+
+	            </div>
 
         </div>
       </div>
