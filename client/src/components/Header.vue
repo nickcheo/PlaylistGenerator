cf605484132996d08cca24cf503cdb0aee59e75f
Adam Kelch <askelch@gmail.com>
2022-08-30 14:41:07 -0400

    build for scope revision

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -59,7 +59,7 @@ export default {
     getSpotifyLogin: async () => {
       /* eslint-disable */
       const querystring = require('querystring')
-      const scope = 'user-read-private user-read-email user-library-read user-top-read playlist-modify-public playlist-modify-private';
+      const scope = 'user-read-private user-library-read user-top-read playlist-modify-public playlist-modify-private';
       console.log('login func heree')
 
       function generateRandomString(n)
